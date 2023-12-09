8bd5817d55609a5ebf11f42eacd72da054230b6a
nikitamessi69 <nzhuravlev92@gmail.com>
2023-12-09 14:32:44 +0300

    rework

--- theDogAPI/test_images.py
+++ theDogAPI/test_images.py
@@ -1,13 +1,11 @@
 import jsonschema
 import pytest
 import requests
-import os
 import json
+import os
 from jsonschema import validate
 from config import *
-from schema.get_schema import schema
 from schema.search_schema import schema
-from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class TestDogAPI:
@@ -46,7 +44,7 @@ class TestDogAPI:
         1. Статус код
         2. Проверяем какой тип изображения jpg/png
         3. Данным методом можно проверить любой тип поля в ответе JSON
-        4. Для проверок можно изпользовать параметризацию параметра GET-запроса"""
+        4. Для проверок можно использовать параметризацию параметра GET-запроса"""
 
         # Параметры запроса
         search_url = URL + "v1/images/search"
@@ -70,22 +68,10 @@ class TestDogAPI:
         2. Валидируем JSON схему """
         search_url = URL + "v1/images"
 
-        # Необязательные параметры
-        data = {
-            'limit': 10,
-            'page': 0,
-            'order': 'DESC'
-        }
-        response = requests.get(search_url, params=data, headers=headers_get)
+        response = requests.get(search_url, headers=headers_get)
 
         # Тест статус кода
         assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
-
-        # Валидация по JSON-схеме
-        try:
-            validate(instance=response.json(), schema=schema)
-        except jsonschema.exceptions.ValidationError as e:
-            pytest.fail(f"Response schema validation error: {e}")
 
     def test_get_my_images_without_token(self):
         """ В данном тесте проверяем:
@@ -110,7 +96,8 @@ class TestDogAPI:
         assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
 
         # Валидация лимитов в JSON ответе
-        assert len(response.json()) == limit, f"Expected {limit} objects in the response, but got {len(response.json())}"
+        assert len(response.json()) == limit, \
+            f"Expected {limit} objects in the response, but got {len(response.json())}"
 
     def test_get_image_by_id(self, headers_get):
         """ В данном тесте проверяем:
@@ -129,22 +116,52 @@ class TestDogAPI:
 
     def test_create_image(self, headers_post):
         upload_url = URL + "v1/images/upload"
-        img_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'img/pome.png'))
 
-        # Создаем объект MultipartEncoder
-        multipart_data = MultipartEncoder(
-            fields={'file': ('pome.png', open(img_path, 'rb'))}
-        )
-
-        # Отправляем POST-запрос с использованием MultipartEncoder
-        response = requests.post(
-            upload_url,
-            headers={**headers_post, 'Content-Type': multipart_data.content_type},
-            data=multipart_data
-        )
+        # Добавим необязательные параметры
+        payload = {'sub_id': 'pomeranian',
+                   'breed_ids': '21'}
+        # Загрузим изображение в запрос
+        files = [('file', ('pome.png', open('img/pome.png', 'rb'), 'image/png'))]
+        response = requests.request("POST", upload_url, headers=headers_post, data=payload, files=files)
 
         # Проверка статус кода
         assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}"
+
+    def test_create_image_with_breed(self, headers_post, headers_get):
+        # Загрузим изображение в запрос
+        upload_url = URL + "v1/images/upload"
+        files = [('file', ('pome.png', open('img/pome.png', 'rb'), 'image/png'))]
+        response = requests.request("POST", upload_url, headers=headers_post, files=files)
+
+        # Вытащим image_id для создания пары с породой собаки
+        response_json = response.json()
+        image_id = response_json["id"]
+        print("Созданный image_id изображения:", image_id)
+
+        # Создадим последующий POST запрос с указанием image_id и breed_id
+        upload_new_url = URL + f"v1/images/{image_id}/breeds"
+
+        # Добавим обязательный параметр BREED_ID из конфига
+        payload = json.dumps({
+            "breed_id": BREED_ID
+        })
+        # Загрузим изображение в запрос
+        response = requests.request("POST", upload_new_url, headers=headers_get, data=payload)
+
+        # Проверка статус кода
+        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
+
+        # Проверить, что id установлено, как в body
+        response_json = response.json()
+        assert response_json.get('id') == BREED_ID, f"Expected breed_id {BREED_ID} not found in JSON response!"
+
+        # Проверим, что наша порода с ID совпадает BREED_ID и image_id
+        search_url = URL + f"v1/images/{image_id}"
+        response = requests.get(search_url, headers=headers_get)
+        json_response = response.json()
+        assert json_response["breeds"][0]["id"] == int(BREED_ID) and json_response["id"] == image_id, (
+            f"Value of 'id' in 'breeds' is not equal to {BREED_ID} "
+            f"and id {image_id}")
 
     def test_create_image_invalid_format(self, headers_post):
         """ В данном тесте проверяем:
@@ -162,20 +179,6 @@ class TestDogAPI:
         # Проверка статус кода
         assert response.status_code == 400, f"Expected status code 400, but got {response.status_code}"
 
-    def test_create_image_with_existed_id(self, headers_post):
-        """ В данном тесте проверяем:
-        1. Статус код
-        2. Создать изображение с image_id и породой """
-        upload_url = URL + f"v1/images/{IMAGE_ID}"
-        data = {
-            'breed_id': 10
-        }
-        # Отправляем POST-запрос
-        response = requests.post(upload_url, headers=headers_post, data=data)
-
-        # Проверка статус кода
-        assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}"
-
     def test_create_image_with_other_user_id(self, headers_get):
         """ В данном тесте проверяем:
         1. Статус код 400
@@ -187,18 +190,44 @@ class TestDogAPI:
         })
         # Отправляем POST-запрос
         response = requests.post(upload_url, headers=headers_get, data=payload)
-        print(response.text)
 
         # Проверка статус кода
         assert response.status_code == 401, f"Expected status code 401, but got {response.status_code}"
 
-    def test_delete_image_by_id(self, headers_get):
+    def test_delete_image_by_id(self, headers_get, headers_post):
         """ В данном тесте проверяем:
                 1. Статус код
-                2. Удалить изобрадение по image_id из своих файлов """
+                2. Получить список своих фото
+                3. Удалить изображение по image_id из своих файлов
+                4. Проверить, что файл удален """
 
-        search_url = URL + f"v1/images/shceRnA48"
+        # Создание нового изображения и получение его image_id
+        upload_url = URL + "v1/images/upload"
+        files = files = [('file', ('dobermann.jpg', open('img/dobermann.jpg', 'rb'), 'image/jpeg'))]
+        response = requests.request("POST", upload_url, headers=headers_post, files=files)
+
+        # Вытаскиваем image_id
+        response_json = response.json()
+        image_id = response_json["id"]
+        print("Созданный image_id изображения:", image_id)
+
+        # Проверяем, что image_id он есть в ответе GET
+        search_url = URL + f"v1/images/{image_id}"
+        response = requests.get(search_url, headers=headers_get)
+        response_json = response.json()
+        assert response_json.get('id') == image_id, f"Expected image_id {image_id} not found in JSON response!"
+
+        # Теперь удалим изображение по image_id
+        search_url = URL + f"v1/images/{image_id}"
         response = requests.delete(search_url, headers=headers_get)
 
-        # Тест статус кода
+        # Тест статус кода (Что изображение удалено)
         assert response.status_code == 204, f"Expected status code 200, but got {response.status_code}"
+
+        # Проверим, что изображение удалено
+        search_url = URL + f"v1/images/{image_id}"
+        response = requests.get(search_url, headers=headers_get)
+        print("Сообщение об успешном удалении:", response.text)
+
+        # Тест статус кода (Ожидаем 400, так как изображение удалено)
+        assert response.status_code == 400, f"Expected status code 400, but got {response.status_code}"
