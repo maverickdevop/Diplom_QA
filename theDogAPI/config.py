959f7ec03bc280d73c19ed28721d64ff9cc2d2ee
maverickdevop <57834199+maverickdevop@users.noreply.github.com>
2024-09-06 00:40:20 +0300

    Update config.py

--- theDogAPI/config.py
+++ theDogAPI/config.py
@@ -1,5 +1,4 @@
 URL = "https://api.thedogapi.com/"
-API_KEY = "live_JuNyiP0BfxFXPvMlakGLqXkwxqvmfXhkEkYuXR5jcvEJZqCyOquqtEmZM99ji8Ba"
 IMAGE_ID = "IBkYVm4v1"
 OTHER_IMAGE_ID = "tmj_UmLCS"
 BREED_ID = 10
